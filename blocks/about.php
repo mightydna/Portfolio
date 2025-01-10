1b8ea37c2a8bcbf1c6ca2ec4db576ac1cc68feb4
Dominik <dominiknagel@gmx.net>
2025-01-10 12:59:39 +0100

    First full pass of the about section

--- blocks/about.php
+++ blocks/about.php
@@ -2,7 +2,7 @@
     <div id="about_text_container">
         <h2 id="about_header">Über Mich</h2>
         <div id="about_header_bar"></div>
-        <p>
+        <p id="about_desc_text">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
         <br>
         Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
@@ -10,7 +10,7 @@
         </p>
     </div>
     <div id="about_img_container">
-        <img src="../resources/images/Hackerman2.webp">
+        <img src="../resources/images/Hackerman2.webp" id="about_img">
         <div id="about_img_bar"></div>
     </div>
 </section>
