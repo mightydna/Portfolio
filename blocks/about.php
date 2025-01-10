3b8266f01943b970fdecd9d6c79fc9f9c3aa137c
Dominik <dominiknagel@gmx.net>
2025-01-10 12:09:37 +0100

    Added about section with structure

---
+++ blocks/about.php
@@ -0,0 +1,16 @@
+<section id="about">
+    <div id="about_text_container">
+        <h2 id="about_header">Über Mich</h2>
+        <div id="about_header_bar"></div>
+        <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        <br>
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+    </div>
+    <div id="about_img_container">
+        <img src="../resources/images/Hackerman2.webp">
+        <div id="about_img_bar"></div>
+    </div>
+</section>
