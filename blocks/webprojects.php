8e76fd6b00382b55322ee004c27991f0997f0e07
Dominik <dominiknagel@gmx.net>
2025-01-13 13:23:19 +0100

    added overlay to projects

--- blocks/webprojects.php
+++ blocks/webprojects.php
@@ -1,15 +1,21 @@
 <section id="projects">
-    <div id="pprojects_header_box">
+    <div id="projects_header_box">
         <h2 id="projects_header">Meine Projekte</h2>
         <div id="projects_header_bar"></div>
     </div>
     <div id="projects_main_container">
         <div class="project_box">
-            <img src="" id="project_box_img">
-            <h3 class="project_box_header"></h3>
-            <p id="project_box_text"></p>
-            <button class="projects_live_button"></button>
-            <button class="projects_github_button"></button>
+            <img src="" class="project_box_img">
+            <div class="project_box_overlay">
+                <div>
+                    <h3 class="project_box_header">Beispiel</h3>
+                    <p class="project_box_text">Beispieltext</p>
+                    <div class="projects_button_container">
+                        <button class="projects_live_button">Live</button>
+                        <button class="projects_github_button">GiTHUB</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div id="projects_bottom_bars_container">
