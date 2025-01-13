7af53d1653a3e8cf5622dc3a670ed48ca7c92f56
Dominik <dominiknagel@gmx.net>
2025-01-13 11:03:48 +0100

    First finished pass of the skills section

--- blocks/skills.php
+++ blocks/skills.php
@@ -9,8 +9,8 @@
         <p id="skills_subtext">Ich konnte bereits viel Erfahrung in der Entwicklung von Projekten mit Frontend- und Backend-Technologien sowie verschiedenen Frameworks sammeln.</p>
         <div id="skills_subtext_bar_right"></div>
     </div>
-    <div id="skills_icon_line_1">
-        <div id="skills_icon_line_1_bar_left"></div>
+    <div id="skills_icon_line_1" class="skills_icon_line">
+        <div id="skills_icon_line_1_bar_left" class="skills_icon_bar_left"></div>
         <div class="skill_box_container">
             <div class="skill_box">
                 <img src="../resources/icons/html5-icon.svg" class="skill_icon">
@@ -25,7 +25,47 @@
                 <p class="skill_text">Bootstrap</p>
             </div>
         </div>
-        <div id="skills_icon_line_1_bar_right"></div>
+        <div id="skills_icon_line_1_bar_right" class="skills_icon_bar_right"></div>
     </div>
-
+    <div id="skills_icon_line_2" class="skills_icon_line">
+        <div id="skills_icon_line_2_bar_left" class="skills_icon_bar_left"></div>
+        <div class="skill_box_container">
+            <div class="skill_box">
+                <img src="../resources/icons/js-icon.svg" class="skill_icon">
+                <p class="skill_text">JavaScript</p>
+            </div>
+            <div class="skill_box">
+                <img src="../resources/icons/react-icon.svg" class="skill_icon">
+                <p class="skill_text">React</p>
+            </div>
+            <div class="skill_box">
+                <img src="../resources/icons/git-icon.svg" class="skill_icon">
+                <p class="skill_text">GIT</p>
+            </div>
+        </div>
+        <div id="skills_icon_line_2_bar_right" class="skills_icon_bar_right"></div>
+    </div>
+    <div id="skills_icon_line_3" class="skills_icon_line">
+        <div id="skills_icon_line_3_bar_left" class="skills_icon_bar_left"></div>
+        <div class="skill_box_container">
+            <div class="skill_box">
+                <img src="../resources/icons/MySQL.svg" class="skill_icon">
+                <p class="skill_text">MySQL</p>
+            </div>
+            <div class="skill_box">
+                <img src="../resources/icons/php-icon.svg" class="skill_icon">
+                <p class="skill_text">PHP</p>
+            </div>
+            <div class="skill_box">
+                <img src="../resources/icons/docker-icon.svg" class="skill_icon">
+                <p class="skill_text">Docker</p>
+            </div>
+        </div>
+        <div id="skills_icon_line_3_bar_right" class="skills_icon_bar_right"></div>
+    </div>
+    <div id="skills_bottom_bar"></div>
+    <div id="skills_style_bar_container">
+        <div id="skills_style_bar"></div>
+        <div id="skills_style_bar_empty"></div>
+    </div>
 </section>
