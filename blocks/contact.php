e5a04410ab25c9579598771763656bd4b15e7816
Dominik <dominiknagel@gmx.net>
2025-01-14 15:14:26 +0100

    further refinement of the contact section

--- blocks/contact.php
+++ blocks/contact.php
@@ -3,7 +3,7 @@
         <h2 id="contact_header">Kontakt</h2>
         <div id="contact_header_bar"></div>
         <p id="contact_text">Ich stehe am Anfang meiner Laufbahn als Webentwickler und suche spannende Herausforderungen, bei denen ich meine Skills einbringen und dazulernen kann. Wenn Du jemanden suchst, der motiviert und mit Spaß bei der Sache ist, melde Dich – ich freue mich auf Deine Nachricht!
-            <br>
+            <br><br>
             Du möchtest noch mehr über mich und meine Arbeit sehen? Schau gerne auf meinen Social-Media-Kanälen vorbei und lass Dich inspirieren.</p>
         <div id="contact_linkcontainer">
             <div id="contact_link_icon_bar"></div>
@@ -16,12 +16,22 @@
     </div>
     <div id="form_block">
         <form id="contact_form">
-            <label for="form_name" class="contact_form_labels">Name</label>
-            <input type="text" id="form_name" name="form_name">
-            <label for="form_mail" class="contact_form_labels">E-Mail</label>
-            <input type="email" id="form_mail" name="form_mail">
-            <label for="form_msg" class="contact_form_labels">Deine Nachricht</label>
-            <textarea id="form_msg" name="form_msg"></textarea>
+            <div class="form_box">
+                <label for="form_name" class="contact_form_labels">Name</label>
+                <input type="text" id="form_name" class="form_field" name="form_name" required>
+            </div>
+            <div class="form_box">
+                <label for="form_mail" class="contact_form_labels">E-Mail</label>
+                <input type="email" id="form_mail" class="form_field" name="form_mail">
+            </div>
+            <div class="form_box">
+                <label for="form_msg" class="contact_form_labels">Deine Nachricht</label>
+                <textarea id="form_msg" name="form_msg"></textarea>
+            </div>
+            <div id="form_dataprot_container">
+                <input type="checkbox" id="form_checkbox">
+                <p id="form_dataprot_text">Ich stimme zu, dass meine Daten zur Bearbeitung meiner Anfrage verwendet werden. Weitere Infos in der <a href="" id="form_dataprot_link">Datenschutzerklärung</a>.</p>
+            </div>
             <input type="submit" id="form_submit" name="form_submit" value="Absenden">
         </form>
     </div>
