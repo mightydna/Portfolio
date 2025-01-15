850ef32b83be11404c5a6274533968a3a33cf166
Dominik <dominiknagel@gmx.net>
2025-01-15 17:40:28 +0100

    changed menu sorting order and added anchors

--- blocks/main/header.php
+++ blocks/main/header.php
@@ -2,11 +2,11 @@
     <p id="logo">DOMINIK</p>
     <nav id="menu">
         <ul>
-            <li><div class="menu_box"><a href="">Über Mich</a></div></li>
-            <li><div class="menu_box"><a href="">Meine Skills</a></div></li>
-            <li><div class="menu_box"><a href="">Meine Erfahrung</a></div></li>
-            <li><div class="menu_box"><a href="">Meine Projekte</a></div></li>
-            <li><div class="menu_box"><a href="">Kontakt</a></div></li>
+            <li><div class="menu_box"><a href="#about">Über Mich</a></div></li>
+            <li><div class="menu_box"><a href="#skills">Meine Skills</a></div></li>
+            <li><div class="menu_box"><a href="#projects">Meine Projekte</a></div></li>
+            <li><div class="menu_box"><a href="#experience">Meine Erfahrung</a></div></li>
+            <li><div class="menu_box"><a href="#contact">Kontakt</a></div></li>
         </ul>
     </nav>
 </header>
