381925c90e60f244e34d3b05d64a13faace0d491
Dominik <dominiknagel@gmx.net>
2025-01-16 15:11:58 +0100

    first layout & design for the certificates page

--- blocks/certificates.php
+++ blocks/certificates.php
@@ -1,16 +1,67 @@
 <section id="certificates">
-    <div id="certificates_header_container">
-        <h2 id="certificates_header">Weitere Skills & Zertifikate</h2>
-        <div id="certificates_header_bar"></div>
-    </div> Grün      
-
-    Skills:
-- SCRUM Master
-- Office skills
-- 
-
-Zertifikate:
-- Webdev
-- SCRUM Master
-
+    <div id="certificates_skill_block">
+        <div id="certificates_skills_desctext_container">
+            <h2 id="certificates_header">Weitere Skills & Zertifikate</h2>
+            <div id="certificates_header_bar"></div>
+            <p id="certificates_skills_desctext">Weitere Skills die ich erworben habe eventuell ein längerer Text hier damit man sehen kann wieviel Platz benötigt wird dafür</p>
+        </div>
+        <div id="certificates_skills_container">
+            <div id="certificates_skills_block">
+                <h3 id="certificates_skills_header">Weitere Skills...</h3>
+                <div id="certificates_skills_box">
+                    <div id="certificates_skills_line1">
+                        <div class="certificates_skills_img">
+                            <img src="" class="certificates_skills_icon">
+                            <p class="certificates_skills_name">Figma</p>
+                        </div>
+                        <div class="certificates_skills_img">
+                            <img src="" class="certificates_skills_icon">
+                            <p class="certificates_skills_name">Photoshop</p>
+                        </div>
+                        <div class="certificates_skills_img">
+                            <img src="" class="certificates_skills_icon">
+                            <p class="certificates_skills_name">Balsamiq</p>
+                        </div>
+                    </div>
+                    <div id="certificates_skills_line2">
+                        <div class="certificates_skills_img">
+                            <img src="" class="certificates_skills_icon">
+                            <p class="certificates_skills_name">Unity</p>
+                        </div>
+                        <div class="certificates_skills_img">
+                            <img src="" class="certificates_skills_icon">
+                            <p class="certificates_skills_name">MS Office</p>
+                        </div>
+                        <div class="certificates_skills_img">
+                            <img src="" class="certificates_skills_icon">
+                            <p class="certificates_skills_name">JIRA</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="certificates_cert_block">
+        <h3 id="certificates_cert_block_header">... und Zertifikate</h3>
+        <div id="certificates_cert_container">
+            <div id="certificates_cert_line">
+                <div class="certificates_cert_img">
+                    <img src="" class="certificates_cert_icon">
+                    <p class="certificates_cert_name">Full Stack Webdeveloper</p>
+                </div>
+                <div class="certificates_cert_img">
+                    <img src="" class="certificates_cert_icon">
+                    <p class="certificates_cert_name">SCRUM Master</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="certificates_bottom_bar_container1">
+        <div id="certificates_bottom_bar_empty1"></div>
+        <div id="certificates_bottom_bar1"></div>
+    </div>
+    <div id="certificates_bottom_bar_container2">
+        <div id="certificates_bottom_bar_empty2"></div>
+        <div id="certificates_bottom_bar2"></div>
+    </div>
 </section>
