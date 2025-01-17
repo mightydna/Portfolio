9c2d0d1ea803edc68cdbaf2c7568a3a701722683
Dominik <dominiknagel@gmx.net>
2025-01-17 17:23:18 +0100

    added env library & database connection

--- public/cockpit.php
+++ public/cockpit.php
@@ -1,6 +1,25 @@
 <section>
     <?php
-        
+
+        require __DIR__ . '/../vendor/autoload.php';
+
+        use Dotenv\Dotenv;
+
+        // Lade die .env-Datei
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+        $dotenv->load();
+
+        // Zugriff auf Umgebungsvariablen
+        $dbHost = $_ENV['DB_HOST'];
+        $dbUser = $_ENV['DB_USERNAME'];
+        $dbPass = $_ENV['DB_PASSWORD'];
+        $dbname = $_ENV['DB_DATABASE'];
+
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbname);
+        if ($mysqli->connect_errno) {
+            throw new RuntimeException('mysqli-Verbindungsfehler: ' . $mysqli->connect_error);
+        }
 
     ?>
 
--- public/index.php
+++ public/index.php
@@ -6,6 +6,18 @@
     <link rel="stylesheet" href="resources/styles/fonts.css">
     <link rel="stylesheet/less" type="text/css" href="resources/styles/base/all.less" />
     <script src="resources/vendors/less.js-4.2.1/dist/less.js" type="text/javascript"></script>
+
+    <!-- og meta -->
+    <meta property="og:type" value="website" />
+    <meta property="og:title" value="Dominik Nagel - Portfolio" />
+    <meta property="og:description" value="Ich bin begeisterter Webentwickler, schaue dir mein Portfolio an!" />
+    <meta property="og:url" value="nageldominik.de" />
+
+    <!-- twitter meta -->
+    <meta name="twitter:card" value="summary_large_image" />
+    <meta name="twitter:title" value="Dominik Nagel - Portfolio" />
+    <meta name="twitter:description" value="Ich bin begeisterter Webentwickler, schaue dir mein Portfolio an!" />
+
     <title>Mein Portfolio</title>
 </head>
 <body>
@@ -19,6 +31,9 @@
     require '../blocks/experience.php';
     require '../blocks/contact.php';
     require '../blocks/main/footer.php';
+
+
+    require 'cockpit.php';
     ?>
     
 </body>
