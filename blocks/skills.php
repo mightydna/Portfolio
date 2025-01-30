2ffe4b5b8dd95b326d0810d5b28776bb8d83c05c
Dominik <dominiknagel@gmx.net>
2025-01-30 17:02:17 +0100

    Reworked skills section layout & added media queries

--- blocks/skills.php
+++ blocks/skills.php
@@ -1,17 +1,8 @@
 <section id="skills">
-    <div id="skills_header_block">
-        <div id="skills_header_bar_left"></div>
-        <h2 id="skills_header">Meine Webskills</h2>
-        <div id="skills_header_bar_right"></div>
-    </div>
-    <div id="skills_subtext_block">
-        <div id="skills_subtext_bar_left"></div>
-        <p id="skills_subtext">Ich konnte bereits viel Erfahrung in der Entwicklung von Projekten mit Frontend- und Backend-Technologien sowie verschiedenen Frameworks sammeln.</p>
-        <div id="skills_subtext_bar_right"></div>
-    </div>
-    <div id="skills_icon_line_1" class="skills_icon_line">
-        <div id="skills_icon_line_1_bar_left" class="skills_icon_bar_left"></div>
-        <div class="skill_box_container">
+    <div id="skills_container">
+        <h2 id="skills_header">Meine Webskills</h2> 
+        <p id="skills_subtext">Ich konnte bereits viel Erfahrung in der Entwicklung von Projekten mit Frontend- und Backend-Technologien sowie verschiedenen Frameworks sammeln.</p>   
+        <div class="skill_grid_container">
             <div class="skill_box">
                 <img src="resources/icons/html5-icon.svg" class="skill_icon">
                 <p class="skill_text">HTML</p>
@@ -24,12 +15,6 @@
                 <img src="resources/icons/bootstrap-icon.svg" class="skill_icon">
                 <p class="skill_text">Bootstrap</p>
             </div>
-        </div>
-        <div id="skills_icon_line_1_bar_right" class="skills_icon_bar_right"></div>
-    </div>
-    <div id="skills_icon_line_2" class="skills_icon_line">
-        <div id="skills_icon_line_2_bar_left" class="skills_icon_bar_left"></div>
-        <div class="skill_box_container">
             <div class="skill_box">
                 <img src="resources/icons/js-icon.svg" class="skill_icon">
                 <p class="skill_text">JavaScript</p>
@@ -42,12 +27,6 @@
                 <img src="resources/icons/git-icon.svg" class="skill_icon">
                 <p class="skill_text">GIT</p>
             </div>
-        </div>
-        <div id="skills_icon_line_2_bar_right" class="skills_icon_bar_right"></div>
-    </div>
-    <div id="skills_icon_line_3" class="skills_icon_line">
-        <div id="skills_icon_line_3_bar_left" class="skills_icon_bar_left"></div>
-        <div class="skill_box_container">
             <div class="skill_box">
                 <img src="resources/icons/MySQL.svg" class="skill_icon">
                 <p class="skill_text">MySQL</p>
@@ -61,9 +40,7 @@
                 <p class="skill_text">Docker</p>
             </div>
         </div>
-        <div id="skills_icon_line_3_bar_right" class="skills_icon_bar_right"></div>
-    </div>
-    <div id="skills_bottom_bar"></div>
+    </div> 
     <div id="skills_style_bar_container">
         <div id="skills_style_bar"></div>
         <div id="skills_style_bar_empty"></div>
