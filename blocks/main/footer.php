7e63878781ef3ecafe910f11809a8e7a43f62293
Dominik <dominiknagel@gmx.net>
2025-01-31 13:51:42 +0100

    Adjusted footer layout & added footer mediaqueries

--- blocks/main/footer.php
+++ blocks/main/footer.php
@@ -5,4 +5,11 @@
         <p id="footer_copyright_text">@ Dominik 2025</p>
         <img src="resources/icons/arrow-up-icon.svg" id="footer_b2t">
     </div>
+    <div id="footer_elements_container_small">
+        <div id="footer_logo_container_small">
+            <p id="footer_logo_small">DOMINIK</p>
+            <p id="footer_copyright_text_small">@ Dominik 2025</p>
+        </div>
+        <img src="resources/icons/arrow-up-icon.svg" id="footer_b2t_small">
+    </div>
 </footer>
