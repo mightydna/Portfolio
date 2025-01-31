77214f724614171381dd8dac9b172bb9fd0d9a67
Dominik <dominiknagel@gmx.net>
2025-01-31 12:58:27 +0100

    added mediaquery for contact section & layout rework

--- blocks/contact.php
+++ blocks/contact.php
@@ -2,9 +2,8 @@
     <div id="contact_textblock">
         <h2 id="contact_header">Kontakt</h2>
         <div id="contact_header_bar"></div>
-        <p id="contact_text">Ich stehe am Anfang meiner Laufbahn als Webentwickler und suche spannende Herausforderungen, bei denen ich meine Skills einbringen und dazulernen kann. Wenn Du jemanden suchst, der motiviert und mit Spaß bei der Sache ist, melde Dich – ich freue mich auf Deine Nachricht!
-            <br><br>
-            Du möchtest noch mehr über mich und meine Arbeit sehen? Schau gerne auf meinen Social-Media-Kanälen vorbei und lass Dich inspirieren.</p>
+        <p id="contact_text_top">Ich stehe am Anfang meiner Laufbahn als Webentwickler und suche spannende Herausforderungen, bei denen ich meine Skills einbringen und dazulernen kann. Wenn Du jemanden suchst, der motiviert und mit Spaß bei der Sache ist, melde Dich – ich freue mich auf Deine Nachricht!</p>
+        <p id="contact_text_btm">Du möchtest noch mehr über mich und meine Arbeit sehen? Schau gerne auf meinen Social-Media-Kanälen vorbei und lass Dich inspirieren.</p>
         <div id="contact_linkcontainer">
             <div id="contact_link_icon_bar"></div>
             <div id="contact_link_icon_box">
@@ -32,7 +31,21 @@
                 <input type="checkbox" id="form_checkbox" name="form_checkbox" required>
                 <p id="form_dataprot_text">Ich stimme zu, dass meine Daten zur Bearbeitung meiner Anfrage verwendet werden. Weitere Infos in der <a href="" target="_blank" id="form_dataprot_link">Datenschutzerklärung</a>.</p>
             </div>
-            <input type="submit" id="form_submit" name="form_submit" value="Absenden">
+            <div id="form_submit_btn_container">
+                <input type="submit" id="form_submit" name="form_submit" value="Absenden">
+            </div>
         </form>
+    </div>
+    <div id="contact_responsive_box">
+        <div id="contact_responsive_box_bar"></div>
+        <p id="contact_responsive_box_text">Du möchtest noch mehr über mich und meine Arbeit sehen? Schau gerne auf meinen Social-Media-Kanälen vorbei und lass Dich inspirieren.</p>
+        <div id="contact_responsive_box_linkcontainer">
+            <div id="contact_responsive_box_link_icon_bar"></div>
+            <div id="contact_responsive_box_linkbox">
+                <a href="http://www.google.de" target="_blank"><img src="resources/icons/mail-icon.svg" class="contact_responsive_box_link_img"></a>
+                <a href="http://www.google.de" target="_blank"><img src="resources/icons/linkedin-icon.svg" class="contact_responsive_box_link_img"></a>
+                <a href="https://github.com/mightydna" target="_blank"><img src="resources/icons/github-icon.svg" class="contact_responsive_box_link_img"></a>
+            </div>
+        </div>
     </div>
 </section>
