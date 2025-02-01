0c35c6624a09cac97128fba047a1608ac39fd6bf
Dominik <dominiknagel@gmx.net>
2025-02-01 16:20:00 +0100

    rebuilding of the certificates layout & mediaqueries

--- blocks/certificates.php
+++ blocks/certificates.php
@@ -1,57 +1,61 @@
 <section id="certificates">
-    <div id="certificates_skill_block">
-        <div id="certificates_skills_desctext_container">
-            <h2 id="certificates_header">Weitere Skills & Zertifikate</h2>
-            <div id="certificates_header_bar"></div>
-            <p id="certificates_skills_desctext">Weitere Skills die ich erworben habe eventuell ein längerer Text hier damit man sehen kann wieviel Platz benötigt wird dafür</p>
+    
+    <div id="certificates_content_container">
+        <div id="certificates_skill_block">
+            <div id="certificates_skills_desctext_container">
+                <h2 id="certificates_header">Weitere Skills & Zertifikate</h2>
+                <div id="certificates_header_bar"></div>
+                <p id="certificates_skills_desctext">Weitere Skills die ich erworben habe eventuell ein längerer Text hier damit man sehen kann wieviel Platz benötigt wird dafür</p>
+            </div>
         </div>
         <div id="certificates_skills_container">
             <div id="certificates_skills_block">
                 <h3 id="certificates_skills_header">Weitere Skills...</h3>
                 <div id="certificates_skills_grid">
                     <div class="certificates_skills_item">
-                        <img src="../public/resources/icons/figma-icon-one-color.svg" class="certificates_skills_icon">
+                        <img src="resources/icons/figma-icon-one-color.svg" class="certificates_skills_icon">
                         <p class="certificates_skills_name">Figma</p>
                     </div>
                     <div class="certificates_skills_item">
-                        <img src="../public/resources/icons/adobe-photoshop-svgrepo-com.svg" class="certificates_skills_icon">
+                        <img src="resources/icons/adobe-photoshop-svgrepo-com.svg" class="certificates_skills_icon">
                         <p class="certificates_skills_name">Photoshop</p>
                     </div>
                     <div class="certificates_skills_item">
-                        <img src="../public/resources/icons/smileyface-transparent-1080x1080.svg" class="certificates_skills_icon">
+                        <img src="resources/icons/smileyface-transparent-1080x1080.svg" class="certificates_skills_icon">
                         <p class="certificates_skills_name">Balsamiq</p>
                     </div>
                     <div class="certificates_skills_item">
-                        <img src="../public/resources/icons/unity-69.svg" class="certificates_skills_icon">
+                        <img src="resources/icons/unity-69.svg" class="certificates_skills_icon">
                         <p class="certificates_skills_name">Unity</p>
                     </div>
                     <div class="certificates_skills_item">
-                        <img src="../public/resources/icons/office-1.svg" class="certificates_skills_icon">
+                        <img src="resources/icons/office-1.svg" class="certificates_skills_icon">
                         <p class="certificates_skills_name">MS Office</p>
                     </div>
                     <div class="certificates_skills_item">
-                        <img src="../public/resources/icons/jira-1.svg" class="certificates_skills_icon">
+                        <img src="resources/icons/jira-1.svg" class="certificates_skills_icon">
                         <p class="certificates_skills_name">JIRA</p>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div id="certificates_cert_block">
-        <h3 id="certificates_cert_block_header">... und Zertifikate</h3>
-        <div id="certificates_cert_container">
-            <div id="certificates_cert_line">
-                <div class="certificates_cert_img">
-                    <img src="" class="certificates_cert_icon">
-                    <p class="certificates_cert_name">Full Stack Webdeveloper</p>
-                </div>
-                <div class="certificates_cert_img">
-                    <img src="../public/resources/icons/professional-scrum-master-i.svg" class="certificates_cert_icon">
-                    <p class="certificates_cert_name">SCRUM Master</p>
+            <div id="certificates_cert_block">
+                <h3 id="certificates_cert_block_header">... und Zertifikate</h3>
+                <div id="certificates_cert_container">
+                    <div id="certificates_cert_line">
+                        <div class="certificates_cert_img">
+                            <img src="" class="certificates_cert_icon">
+                            <p class="certificates_cert_name">Full Stack Webdeveloper</p>
+                        </div>
+                        <div class="certificates_cert_img">
+                            <img src="resources/icons/professional-scrum-master-i.svg" class="certificates_cert_icon">
+                            <p class="certificates_cert_name">SCRUM Master</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
     <div id="certificates_bottom_bar_container1">
         <div id="certificates_bottom_bar_empty1"></div>
         <div id="certificates_bottom_bar1"></div>
