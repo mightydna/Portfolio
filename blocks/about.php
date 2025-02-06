bab2a37523ac8a0cc16cd9587d628d5a10844ad1
Dominik <dominiknagel@gmx.net>
2025-02-06 22:32:27 +0100

    small text changes in about section

--- blocks/about.php
+++ blocks/about.php
@@ -4,7 +4,7 @@
         <div id="about_header_bar"></div>
         <p id="about_desc_text">
         Hey, ich bin Dominik! Schon seit meiner Kindheit haben mich Games und digitale Medien fasziniert – heute darf ich genau das als Game Designer und Webentwickler beruflich ausleben. </br>
-        Was als Hobby begann, wurde schnell zur Leidenschaft, und mittlerweile bringe ich viel Erfahrung in der Spieleentwicklung mit. Im Webdesign stehe ich noch am Anfang, freue mich aber darauf, meine Fähigkeiten weiterzuentwickeln und spannende Projekte umzusetzen. Egal ob kreative Konzepte, technische Umsetzung oder UX – ich liebe es, Ideen zum Leben zu erwecken und digitale Erlebnisse zu gestalten.
+        Was als Hobby begann, wurde schnell zur Leidenschaft, und mittlerweile bringe ich viel Erfahrung aus der Spieleentwicklung mit.</br></br> Im Webdevelopment stehe ich noch am Anfang, freue mich aber darauf, meine Fähigkeiten weiterzuentwickeln und spannende Projekte umzusetzen. Egal ob kreative Konzepte, technische Umsetzung oder UX – ich liebe es, Ideen zum Leben zu erwecken und digitale Erlebnisse zu gestalten.
         </p>
     </div>
     <div id="about_img_container">
