f030a61a5869ecfe63563fe3c3b6180d2cedf5be
Dominik <dominiknagel@gmx.net>
2025-02-07 13:53:10 +0100

    Moved WIP header shrink code to separate js file

--- public/resources/scripts/main.js
+++ public/resources/scripts/main.js
@@ -8,8 +8,7 @@ var burger = document.querySelector("#burgermenu_main");
 var burgermenu_overlay = document.querySelector("#burgermenu_overlay");
 var overlay_close = document.querySelector("#bmenu_overlay_close_btn");
 var body = document.querySelector("body");
-var menu_btn_about = document.querySelector("#menu_btn_about");
-var menu_btn_contact = document.querySelector("#menu_btn_contact");
+
 // Burgermenu Buttons //
 var bmenu_btn_about = document.querySelector("#burger_menu_btn_about");
 var bmenu_btn_skills = document.querySelector("#burger_menu_btn_skills");
@@ -17,7 +16,7 @@ var bmenu_btn_projects = document.querySelector("#burger_menu_btn_projects");
 var bmenu_btn_experience = document.querySelector("#burger_menu_btn_experience");
 var bmenu_btn_contact = document.querySelector("#burger_menu_btn_contact");
 var bmenu_btn_b2t = document.querySelector("#bmenu_b2t");
-//var header_size = 1;
+
 var dataprot_btn = document.querySelector("#form_dataprot_link");
 var dataprot_overlay = document.querySelector("#dataprot_overlay");
 var dataprot_overlay_close_btn = document.querySelector("#dataprot_overlay_close_btn");
@@ -44,6 +43,9 @@ imprint_btn.addEventListener("click", toggleImprintOverlay);
 imprint_btn_small.addEventListener("click", toggleImprintOverlay);
 imprint_overlay_close_btn.addEventListener("click", toggleImprintOverlay);
 
+
+// Funktionen //
+// Overlay Toggle-Funktionen //
 function toggleOverlay() {
     burgermenu_overlay.classList.toggle("bm_visible");
     body.classList.toggle("no_scroll");
@@ -59,65 +61,6 @@ function toggleImprintOverlay() {
     imprint_overlay.classList.toggle("imprint_visible");
     body.classList.toggle("no_scroll");
 }
-
-/*
-menu_btn_contact.addEventListener("click", function() {
-    if (header_size == 1) {
-        var contact = document.querySelector("#contact");
-        contact.style.scrollMarginTop = "10vh";
-        console.log(about.style);
-    } else if (header_size == 0) {
-        var contact = document.querySelector("#contact");
-        contact.style.scrollMarginTop = "15vh";
-        window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" });
-        setTimeout(() => {
-            window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" });
-            console.log("warte...");
-        }, 900);
-        
-        header_placeholder.style.display = "flex";
-        console.log(about.style);
-    }
-})
-
-
-menu_btn_about.addEventListener("click", function() {
-    if (header_size == 1) {
-        var about = document.querySelector("#about");
-        about.style.scrollMarginTop = "10vh";
-        console.log(about.style);
-    } else if (header_size == 0) {
-        var about = document.querySelector("#about");
-        about.style.scrollMarginTop = "5vh";
-        console.log(about.style);
-    }
-})
-
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("#header");
-    console.log(window.scrollY);
-    if (window.scrollY > 4000) {
-        if (header_size == 0) {
-            header.classList.remove("shrink");
-            header_size = 1;
-        }
-        
-    } else if (window.scrollY > 450 ){
-        if (header_size == 1) {
-            header.classList.add("shrink");
-            header_size = 0;
-        }
-        
-    } else if (window.scrollY < 200) {
-        if (header_size == 0) {
-            header.classList.remove("shrink");
-            header_size = 1;
-            console.log("wtf");
-        }
-        
-    }
-});
-*/
 
 
 // Kontaktformular Funktionalität //
