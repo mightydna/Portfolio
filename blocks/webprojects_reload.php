4b5f802b923ab72be5452d9471167bc6bf0ac0f5
Dominik <dominiknagel@gmx.net>
2025-02-08 16:41:27 +0100

    added project display option to project reload page

--- blocks/webprojects_reload.php
+++ blocks/webprojects_reload.php
@@ -11,6 +11,26 @@ echo '<div class="projects_wrapper">';
 
 while ($row = $result->fetch_assoc()) {
     $image = base64_encode($row['image']);
+    if ($row['gitlink'] == "") {
+    $name = ($lang == "en") ? htmlspecialchars($row['name_en']) : htmlspecialchars($row['name']);
+    $desc = ($lang == "en") ? htmlspecialchars($row['description_en']) : htmlspecialchars($row['description']);
+
+    echo '<div class="project_box">
+        <div class="project_box_img_container">
+            <img src="data:image/png;base64,' . $image . '" class="project_box_img" alt="Projektbild">
+        </div>
+        <div class="project_box_overlay">
+            <div>
+                <h3 class="project_box_header">' . $name . '</h3>
+                <p class="project_box_text">' . $desc . '</p>
+                <span class="projects_button_container">
+                    <a class="projects_live_button" href="' . htmlspecialchars($row['livelink']) . '" target="_blank">Link</a>
+
+                </span>
+            </div>
+        </div>
+      </div>';
+    } else {
     $name = ($lang == "en") ? htmlspecialchars($row['name_en']) : htmlspecialchars($row['name']);
     $desc = ($lang == "en") ? htmlspecialchars($row['description_en']) : htmlspecialchars($row['description']);
 
@@ -29,6 +49,7 @@ while ($row = $result->fetch_assoc()) {
             </div>
         </div>
       </div>';
+    }
 }
 echo '</div>';
 echo '
